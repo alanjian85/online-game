2a6ede3be96e4fd29f5512ecb3170a8e3d997a46
alanjian85 <alanjian85@outlook.com>
2022-06-02 14:10:16 +0800

    Implement thread-safe player

--- common/Player.hpp
+++ common/Player.hpp
@@ -1,11 +1,26 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
+#include <mutex>
+
 class Player {
 public:
-    void setPosition(int x, int y);
+    void setPosition(int x, int y) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_x = x;
+        m_y = y;
+    }
 
-    void getPosition(int& x, int& y) const;
+    void getPosition(int& x, int& y) const {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        x = m_x;
+        y = m_y;
+    }
+
+private:
+    mutable std::mutex m_mutex;
+    int m_x;
+    int m_y;
 };
 
 #endif
